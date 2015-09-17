3b7ca4567f40d1620345fec606035c3d805e23dd
PJ Miranda <pendelhaven3@gmail.com>
2015-09-17 11:31:16 +0800

    Add version to window title

--- src/main/java/com/pj/loantracker/controller/MainMenuController.java
+++ src/main/java/com/pj/loantracker/controller/MainMenuController.java
@@ -1,5 +1,7 @@
 package com.pj.loantracker.controller;
 
+import java.util.ResourceBundle;
+
 import org.springframework.stereotype.Controller;
 
 import javafx.fxml.FXML;
@@ -7,9 +9,11 @@ import javafx.fxml.FXML;
 @Controller
 public class MainMenuController extends AbstractController {
 
+	private ResourceBundle bundle = ResourceBundle.getBundle("application");
+	
 	@Override
 	public void updateDisplay() {
-		stageController.setTitle("Loan Tracker");
+		stageController.setTitle(bundle.getString("application.title"));
 	}
 
 	@FXML public void goToClientListScreen() {
